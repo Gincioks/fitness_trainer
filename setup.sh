008e1049fbb7bb980f06b19629b6282c7bbe0662
Gintaras Ružauskas <38226841+Gincioks@users.noreply.github.com>
2024-04-15 21:55:45 +0300

    update

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 wget https://repo.anaconda.com/archive/Anaconda3-2024.02-1-Linux-x86_64.sh && 
 bash Anaconda3-2024.02-1-Linux-x86_64.sh -b -u -p $HOME/anaconda3 && eval "$($HOME/anaconda3/bin/conda shell.bash hook)" && conda init &&
-cd app && conda env create -f environment.yml 
+cd app && conda env create -f environment.yml && conda activate virtualFit && streamlit run app.py 
